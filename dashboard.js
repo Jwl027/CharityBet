8fd7128142000f0b6028c99257ed2b598b55e787
Jwl027 <jwl027@ucsd.edu>
2018-02-03 13:19:12 -0800

    Added links to dashboard - need to do accept requests in order to move on

--- dashboard.js
+++ dashboard.js
@@ -60,46 +60,115 @@ function creator(){
 
 
   			});
+
+
   			ref.child('bets').once('value',snap=>{
+  				console.log(snap.val());
+  			
+  				snap.forEach(function(child){
+  					console.log(child.val());
+  					//console.log(child.key);
+  					betArray.push(child.key);
+  					console.log(1);
+
+
+  					// var $divUserName = $("<div>", {"class": "userNameL"});
+					// var $divOponentName = $("<div>", {"class": "userNameR"});
+					var $divDescription = $("<div>", {"class": "description"});
+					var $divMiddleMan = $("<div>", {"class": "middleMan"});
+					// var $divAmount = $("<div>", {"class": "amount"});
+					var $divDetails = $("<div>", {"class": "details"});
+
+					var $userName = $("<h3>", {"class": "userNameText"});
+					var $oponentName = $("<h3>", {"class": "oponentNameText"});
+					var $middleManName = $("<h3>", {"class": "middleManText"});
+					var $amount = $("<h3>", {"class": "amountText","id" : "amount" + i});
+					var $description = $("<h3>", {"class" : "descriptionText", "id" : "description" + i});
+
+					//TODO:CHANGE OPPONENT NAME and MIDDLE MAN NAME DYNAMIC
+					$oponentName.append("OPPENT_NAME_AT_I");
+					$userName.append('USERNAME');
+					$middleManName.append("MIDDLE MAN" + i);
+					$description.append("DESCRIPTION" + i);
+					$amount.append("$ " + i);
+					// $divUserName.append($userName);
+					// $divOponentName.append($oponentName);
+					$divDescription.append($description);
+					// $divAmount.append($amount);
+					$divMiddleMan.append($middleManName);
+
+					//TODO: CHANGE DESCRIPTION DYNAMIC
+					//		Change AMOUNT DYNAMIC
+
+					$divDetails.append($userName);
+					$divDetails.append($amount);
+					$divDetails.append($oponentName);
+
+
+					var $div = $("<div>", {id: "card" + i, "class": "card"});
+					var $div2 = $("<div>", {id: "innerCard" + i, "class": "inner"});
+
+					// $div2.append($divDescription);
+
+					// $div2.append($divUserName);
+					// $div2.append($divAmount);
+					// $div2.append($divOponentName);
+
+
+					$div2.append($divDescription);
+					$div2.append($divDetails);
+					$div2.append($divMiddleMan);
+					$div.append($div2);
+					$("#middle").append($div);
+	
+
+
+
+
+
+  				});
+
+  			
+			
+
+  			});
+		//Leftside
+		ref.child('middle').once('value',snap=>{
   			console.log(snap.val());
   			
   			snap.forEach(function(child){
-  				console.log(child.val());
-  				//console.log(child.key);
-  				betArray.push(child.key);
-  				console.log(1);
-  				});
 
-  			//RIGHT SIDE
-			for (var i = 0; i < 0;i++) {
-    			var $div = $("<div>", {"class": "requestedBets", "id": "reqBet" + i});
-				var $div2 = $("<div>", {"class": "requestedBetsCard", "id": "reqBetCard" + i});
-				var $div3 = $("<div>", {"class": "requestedBetsDetails", "id": "reqBetDetails" + i});
-				var $div4 = $("<div>", {"class": "requestedBetsDecision", "id": "reqBetDecision" + i});
-				var $div5 = $("<div>", {"class": "requestedBetsCharity", "id": "reqBetCharity" + i});
-				var ref = firebase.database().ref().child('users').child(user).child('requests');
+  				$container = $("<div>", {"class": "midManContain", "id" : "midManContain" + i});
+		$card = $("<div>", {"class": "midManCard", "id": "midManCard" + i});
+		$user1 = $("<div>", {"class": "minManUser1", "id": "minManUser1" + i});
+		$approve = $("<div>", {"class": "minManApprove", "id": "minManApprove" + i});
+		$user2 = $("<div>", {"class": "minManUser2", "id": "minManUser2" + i});
 
-				ref.once('value',snap=>{
-					console.log(snap.val());
-					//TODO: CHANGE .append text to bet description
-					$($div3).append( $("<h1>", {"class": "requestedBetsDetailsText", "id" : "reqBetDetailsText" + i}).append('')    );
-					//TODO: MAKE BUTTON FCNS ACTUALLY WORK
-					$($div4).append( $("<button>", {"class": "reqBetDecApprove", "id" : "reqBetDecApprove" + i}).on("click", buttonApprove()) );
-					$($div4).append( $("<button>", {"class": "reqBetDecDecline", "id" : "reqBetDecDecline" + i}).on("click", buttonDecline()) );
-					//TODO: MAKE CHARITY NAME ACTUALLY APPEAR
-					$($div5).append( $("<h1>", {"class": "requestedBetsCharityText", "id" : "requestedBetsCharityText" + i}).append("Charity Name")    );
-				});
-				$div2.append($div3);
-				$div2.append($div4);
-				$div2.append($div5);
+		//TODO Remove USER1Name  & 2 with actual username
+		$user1.append( $("<h1>", {"class": "minManUser1Text", "id": "minManUser1Text" + i}).append("USER1Name") );
+		$user1.append( $("<input>", {"type": "checkbox", "class": "CheckBox", "id" : "CheckBoxU1" + i}) );
 
-				$div.append($div2);
 
-				$("#right").append($div);
+		$user2.append( $("<h1>", {"class": "minManUser2Text", "id": "minManUser2Text" + i}).append("USER2Name") );
+		$user2.append( $("<input>", {"type": "checkbox", "class": "CheckBox", "id" : "CheckBoxU2" + i}) );
 
-			}
+		$approve.append( $("<button>", {"class": "ApproveButton", "id": "ApproveButton" + i}) );
 
+		$card.append($user1);
+		$card.append($approve);
+		$card.append($user2);
+
+		$container.append($card);
+
+		$("#left").append($container);
   			});
+
+  			
+			
+
+  		});
+		//end of left side
+
   		}
   		else{
   			console.log('not logged in');
@@ -115,7 +184,7 @@ function creator(){
 	//DYNAMICLLY CREATES CARDS FOR MIDDLE ACTIVE BETS
 	//TODO
 	//CHANGE I TO ARRAY SIZE OF ACTIVE BETS
-	for (var i = 0; i < 5; i++) {
+	for (var i = 0; i < 0; i++) {
 		// var $divUserName = $("<div>", {"class": "userNameL"});
 		// var $divOponentName = $("<div>", {"class": "userNameR"});
 		var $divDescription = $("<div>", {"class": "description"});
@@ -173,7 +242,7 @@ function creator(){
 	
 
 	//Left side
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 0; i++) {
 		$container = $("<div>", {"class": "midManContain", "id" : "midManContain" + i});
 		$card = $("<div>", {"class": "midManCard", "id": "midManCard" + i});
 		$user1 = $("<div>", {"class": "minManUser1", "id": "minManUser1" + i});
